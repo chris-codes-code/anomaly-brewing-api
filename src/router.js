6077575b1d35782f1f6ae64c1aa015cc51d4a4fd
Chris Codes <chriscodes.co@gmail.com>
2026-01-01 17:38:54 +0000

    feat: add Express router with route definitions

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+import { getContactURL } from './endpoints/api/contact/url.js';
+import { getPageURL } from './endpoints/content/page/url.js';
+import { getPagesURL } from './endpoints/content/pages/url.js';
+import { getSiteURL } from './endpoints/content/site/url.js';
+import { getSubscribeURL } from './endpoints/api/subscribe/url.js';
+
+import { getSiteController } from './endpoints/content/site/controller.js';
+import { getPageController } from './endpoints/content/page/controller.js';
+import { getPagesController } from './endpoints/content/pages/controller.js';
+
+import { postContactController } from './endpoints/api/contact/controller.js';
+import { postSubscribeController } from './endpoints/api/subscribe/controller.js';
+
+const router = express.Router();
+
+router.get(getSiteURL(), getSiteController);
+router.get(getPageURL(), getPageController);
+router.get(getPagesURL(), getPagesController);
+
+router.post(getContactURL(), postContactController);
+router.post(getSubscribeURL(), postSubscribeController);
+
+export default router;
