66467e5686b58e25e06be03bfc4f20ba037c1cc6
Chris Codes <chriscodes.co@gmail.com>
2026-01-01 17:38:46 +0000

    feat: add application configuration module

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export default {
+  cors: {
+    credentials: process.env.CORS_CREDENTIALS === 'true',
+    methods: (process.env.CORS_METHODS || 'GET,POST,PUT,DELETE,PATCH').split(','),
+    origin: process.env.CORS_ORIGIN,
+  },
+  datocms: {
+    apiToken: process.env.DATOCMS_API_TOKEN,
+  },
+  defaultLocale: process.env.DEFAULT_LOCALE || 'en',
+  emailOctopus: {
+    apiKey: process.env.EMAIL_OCTOPUS_API_KEY,
+    apiEndpoint: process.env.EMAIL_OCTOPUS_API_ENDPOINT,
+  },
+  server: {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3001,
+  },
+  web3Forms: {
+    accessKey: process.env.WEB_3_FORMS_ACCESS_KEY,
+    apiEndpoint: process.env.WEB_3_FORMS_API_ENDPOINT,
+  },
+};
